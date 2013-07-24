daba8b76e1065e80abd297a87d847140ef0ac944
Weston Platter <westonplatter@gmail.com>
2013-07-23 23:42:28 -0600

    finished the button clicks for the UI.

--- javascripts/think602.js
+++ javascripts/think602.js
@@ -1,1 +1,15 @@
+$(document).ready( function(){
 
+  $('.jumbotron a').click(function(e){
+    e.preventDefault();
+    $('#contact').tab('show');
+    $('#tabs > li').removeClass('active');
+  });
+
+  $('#title-click').click(function(e){
+    e.preventDefault();
+    $('#tabs a:first').tab('show');
+    $('#tabs > li').removeClass('active');
+  });
+
+});
