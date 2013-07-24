0beb359b16bc353a04340e921545f9d0e29a3dff
Weston Platter <westonplatter@gmail.com>
2013-07-24 04:21:44 -0600

    frontend styling for pages. need to finish work section.

--- javascripts/think602.js
+++ javascripts/think602.js
@@ -1,15 +1,34 @@
 $(document).ready( function(){
 
+  // the big green "Contact Us" button
+  //
   $('.jumbotron a').click(function(e){
     e.preventDefault();
     $('#contact').tab('show');
     $('#tabs > li').removeClass('active');
   });
 
+  // clicking on think602
+  //
   $('#title-click').click(function(e){
     e.preventDefault();
     $('#tabs a:first').tab('show');
     $('#tabs > li').removeClass('active');
   });
+
+  // "More about process" button
+  //
+  $('a[href="#process"]').click( function(e) {
+    e.preventDefault();
+    $('#tabs li:eq(2) a').tab('show');
+    // move to process description text
+  })
+
+  // "View work" button
+  //
+  $('a[href="#work"]').click( function(e) {
+    e.preventDefault();
+    $('#tabs li:eq(2) a').tab('show');
+  })
 
 });
